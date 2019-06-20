f3a363fbf54e597b70dd500b1bf6d93392ba311e
Brennan Martin <Brennan.Martin@arrow.com>
2019-06-20 10:58:06 -0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// counter project
+(function(){
+
+  let counterValue = 0;
+
+  // get
+  const buttons = document.querySelectorAll('.counterBtn');
+  const counter = document.getElementById('counter');
+
+
+  buttons.forEach(function(btn){
+    btn.addEventListener('click', function(event){
+      // element that we click
+
+      const value = event.target;
+
+      if (value.classList.contains('prevBtn')){
+        counterValue--;
+      } else if (value.classList.contains('nextBtn')){
+        counterValue++;
+      } 
+      counter.textContent = counterValue;
+
+      if(counterValue ===0 ){
+        counter.style.color = '#333333';
+      }
+      else if (counterValue < 0){
+        counter.style.color = 'red';
+      } else if (counterValue > 0){
+        counter.style.color = 'green';
+      }
+
+
+    })
+    
+  })
+
+
+})();
